1d0c4ecc39c6440123e6e66e6cfaff1e726eea80
SuhelMiaBRRI <moynul.bau@gmail.com>
2025-05-07 22:51:11 +0600

    part2

---
+++ RDMS_Day_4-Programing Basics(Part-2)/Data types.R
@@ -0,0 +1,53 @@
+#Data type in R
+#1. Numeric -Integer/Floting data
+#2. String/Character- "Sadan Verma" / 'Sadhan Verma'
+#3. Logical
+
+a=11
+b=7.19
+class(a)
+class(b)
+
+name='Sadhan Verma'
+print(name)
+class(name)
+
+c="52"
+print(c+9)
+print(a+9)
+class(c)
+
+# Logical data type- TRUE, FALSE(T, F)
+a>b
+class(a>b)
+a<b
+x= 19
+y=22-3
+print(x==y)
+print(x!=y)
+print(y>=x)
+
+
+#Logical operators in R
+4==4 && 5==5
+TRUE & TRUE
+T && T
+F && T
+
+#|| Logical operator
+
+x!=y ||x==y
+# Simple mathematical operators in R
+z= 6+ 9-8*(2/3)
+print(z)
+
+w=4+3-3*2/3
+print(w)
+y=4^3
+print(y)
+11/5
+11%%5
+11%/%5
+
+
+
